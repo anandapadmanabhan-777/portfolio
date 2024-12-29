aebbf7221191ee58004eccd10ebc89827dc8b5dc
ANANDAPADMANABHAN S <anandapadmanabhan777@gmail.com>
2024-12-29 15:41:52 +0530

    Update connect.py

--- MyPortfolioWebapp/connect.py
+++ MyPortfolioWebapp/connect.py
@@ -1,34 +1,4 @@
 import streamlit as st 
-
-# def connect():
-#     st.markdown("""
-#     <div class="content-container2">
-#         <h1 class="custom-heading">Contact Information</h1>
-#         <br>    
-#     <div style="text-align: center; display: flex; justify-content: center; gap: 20px; margin-top: 20px;">
-#         <div>
-#             <a href="https://www.linkedin.com/in/anandapadmanabhan-s-43959522a/" target="_blank">
-#                 <img src="https://cdn-icons-png.flaticon.com/512/145/145807.png" alt="LinkedIn Profile" width="50">
-#             </a>
-#             <p style="font-family: 'Lora', sans-serif; font-size: 18px; margin-top: 10px;">LinkedIn</p>
-#         </div>
-#         <div>
-#             <a href="mailto:anandapadmanabhan777@gmail.com">
-#                 <img src="https://cdn-icons-png.flaticon.com/512/552/552486.png" alt="Email Me" width="50">
-#             </a>
-#             <p style="font-family: 'Lora', sans-serif; font-size: 18px; margin-top: 10px;">Email</p>
-#         </div>
-#         <div>
-#             <a href="https://github.com/anandapadmanabhan-777" target="_blank">
-#                 <img src="https://cdn-icons-png.flaticon.com/512/733/733553.png" alt="GitHub Profile" width="50">
-#             </a>
-#             <p style="font-family: 'Lora', sans-serif; font-size: 18px; margin-top: 10px;">GitHub</p>
-#         </div>
-#     </div>
-#     </div>
-#     """, unsafe_allow_html=True)
-
-
 def connect():
     st.markdown("""
     <div class="content-container2" style="margin-bottom: 50px;">
